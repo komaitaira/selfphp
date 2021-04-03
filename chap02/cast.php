c34335fc3cbea6c38efe49e505710a3ffc666fe8
komaitaira <komaitaira@gmail.com>
2021-04-03 14:43:32 +0900

    Initial commit

---
+++ chap02/cast.php
@@ -0,0 +1,16 @@
+<pre>
+<?php
+var_dump((int)1530.95);
+var_dump((int)-1530.95);
+var_dump((int)true);
+var_dump((string)true);
+var_dump((int)false);
+var_dump((string)false);
+var_dump((array)108);
+var_dump((unset)1530.95);
+
+$fruit = ["リンゴ", "みかん", "柿"];
+var_dump($fruit);
+
+$result = `dir`;
+print $result;
