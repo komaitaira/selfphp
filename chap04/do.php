1b6a86fa9a115b24b413c311828e06caade0964d
komaitaira <komaitaira@gmail.com>
2021-04-03 15:13:42 +0900

    while文とdo~while文

--- chap04/do.php
+++ chap04/do.php
@@ -1,6 +1,16 @@
 <?php
-$i = 1;
+$i = 6;
+
+print "do~while文";
+// do~while文は後置判定（条件式にかかわらず必ず1回はループが実行される）
 do {
   print "{$i}番目のループです。<br />";
   $i++;
 } while ($i < 6);
+
+print "while文";
+// while文は前置判定
+while ($i < 6) {
+  print "{$i}番目のループです。<br />";
+  $i++;
+}
