dc55638ae59d884385475600fde88f194e17f7b8
komaitaira <komaitaira@gmail.com>
2021-04-09 23:09:58 +0900

    Add switch文

--- chap04/Answer/practice2.php
+++ chap04/Answer/practice2.php
@@ -1,10 +1,40 @@
 <?php
-  $result = 0;
-  for($i = 0; $i <= 100; $i++) {
-    if($i % 2 === 0) {
-      continue;
-    } else {
-      $result += $i;
+  // $result = 0;
+  // for($i = 0; $i <= 100; $i++) {
+  //   if($i % 2 === 0) {
+  //     continue;
+  //   } else {
+  //     $result += $i;
+  //   }
+  // }
+  // print $result;
+
+  // $result = 0;
+  // $i = 0;
+  // while ($i <= 100) {
+  //   $i++;
+  //   if ($i % 2 === 0) {
+  //     continue;
+  //   } else {
+  //     $result += $i;
+  //   }
+  // }
+  // print $result;
+
+  $languages = ["PHP", "JSP", "ASP", "JavaScript", "VBScript"];
+  foreach($languages as $language) {
+    switch ($language) {
+      case "PHP" :
+      case "JSP" :
+      case "ASP" :
+        print "サーバーサイド技術";
+        break;
+      case "JavaScript" :
+      case "VBScript" :
+        print "クライアントサイドスクリプト";
+        break;
     }
   }
-  print $result;
+  // switch {
+  //   case 
+  // }
